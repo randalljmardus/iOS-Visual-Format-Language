f9c9d466a8e2b7bc40985aefe2306f34398fdda3
Randall Mardus <randalljmardus@gmail.com>
2016-02-25 12:08:27 -0500

    Initial Commit

---
+++ Visual Format Language/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Visual Format Language
+//
+//  Created by Randall Mardus on 2/25/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
