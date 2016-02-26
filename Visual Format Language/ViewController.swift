adfa1c86a2f0fa26473aa614d12f9f8c84498f8c
Randall Mardus <randalljmardus@gmail.com>
2016-02-26 14:46:05 -0500

    addressed conflict with priority

--- Visual Format Language/ViewController.swift
+++ Visual Format Language/ViewController.swift
@@ -21,9 +21,12 @@ class ViewController: UIViewController {
         
         let views = ["myView" : myView]
         
-        let constraints1 = NSLayoutConstraint.constraintsWithVisualFormat("|[myView(100)]", options: .AlignAllTop, metrics: nil, views: views)
+        let constraints1 = NSLayoutConstraint.constraintsWithVisualFormat("|[myView(100,<=50)]", options: .AlignAllTop, metrics: nil, views: views)
         let constraints2 = NSLayoutConstraint.constraintsWithVisualFormat("V:[myView(200)]|", options: .AlignAllTop, metrics: nil, views: views)
     
+        
+        
+        
         NSLayoutConstraint.activateConstraints(constraints1)
         NSLayoutConstraint.activateConstraints(constraints2)
     
